eb6ff854cfea9067112891af53c4f06d7d707098
narunaru0 <h-hirahara@mba.nifty.ne.jp>
2013-08-20 23:47:00 +0900

    馬クラス対応暫定版

--- JV-Relay/Form/MainForm.Designer.cs
+++ JV-Relay/Form/MainForm.Designer.cs
@@ -37,10 +37,11 @@
             this.mainToolStripProgressBar = new System.Windows.Forms.ToolStripProgressBar();
             this.mainToolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
             this.debugGroupBox = new System.Windows.Forms.GroupBox();
+            this.umaFileSaveButton = new System.Windows.Forms.Button();
             this.raceFileSaveButton = new System.Windows.Forms.Button();
             this.quickFileSaveButton = new System.Windows.Forms.Button();
             this.umaGroupBox = new System.Windows.Forms.GroupBox();
-            this.umaFileSaveButton = new System.Windows.Forms.Button();
+            this.umaPostButton = new System.Windows.Forms.Button();
             this.fromLabel = new System.Windows.Forms.Label();
             this.fromTextBox = new System.Windows.Forms.TextBox();
             this.isSetupCheckBox = new System.Windows.Forms.CheckBox();
@@ -70,7 +71,6 @@
             this.helpToolStripSeparator = new System.Windows.Forms.ToolStripSeparator();
             this.aboutBoxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.mainBackgroundWorker = new System.ComponentModel.BackgroundWorker();
-            this.umaPostButton = new System.Windows.Forms.Button();
             this.mainToolStripContainer.BottomToolStripPanel.SuspendLayout();
             this.mainToolStripContainer.ContentPanel.SuspendLayout();
             this.mainToolStripContainer.TopToolStripPanel.SuspendLayout();
@@ -98,11 +98,11 @@
             this.mainToolStripContainer.ContentPanel.Controls.Add(this.quickGroupBox);
             this.mainToolStripContainer.ContentPanel.Controls.Add(this.raceGroupBox);
             this.mainToolStripContainer.ContentPanel.Controls.Add(this.axJVLink);
-            this.mainToolStripContainer.ContentPanel.Size = new System.Drawing.Size(524, 354);
+            this.mainToolStripContainer.ContentPanel.Size = new System.Drawing.Size(524, 402);
             this.mainToolStripContainer.Dock = System.Windows.Forms.DockStyle.Fill;
             this.mainToolStripContainer.Location = new System.Drawing.Point(0, 0);
             this.mainToolStripContainer.Name = "mainToolStripContainer";
-            this.mainToolStripContainer.Size = new System.Drawing.Size(524, 403);
+            this.mainToolStripContainer.Size = new System.Drawing.Size(524, 451);
             this.mainToolStripContainer.TabIndex = 5;
             this.mainToolStripContainer.Text = "toolStripContainer1";
             // 
@@ -138,12 +138,23 @@
             this.debugGroupBox.Controls.Add(this.umaFileSaveButton);
             this.debugGroupBox.Controls.Add(this.raceFileSaveButton);
             this.debugGroupBox.Controls.Add(this.quickFileSaveButton);
+            this.debugGroupBox.Controls.Add(this.isSetupCheckBox);
             this.debugGroupBox.Location = new System.Drawing.Point(12, 269);
             this.debugGroupBox.Name = "debugGroupBox";
-            this.debugGroupBox.Size = new System.Drawing.Size(419, 77);
+            this.debugGroupBox.Size = new System.Drawing.Size(460, 113);
             this.debugGroupBox.TabIndex = 4;
             this.debugGroupBox.TabStop = false;
             this.debugGroupBox.Text = "デバッグ用";
+            // 
+            // umaFileSaveButton
+            // 
+            this.umaFileSaveButton.Location = new System.Drawing.Point(246, 18);
+            this.umaFileSaveButton.Name = "umaFileSaveButton";
+            this.umaFileSaveButton.Size = new System.Drawing.Size(114, 48);
+            this.umaFileSaveButton.TabIndex = 3;
+            this.umaFileSaveButton.Text = "馬情報\r\n（ファイル保存のみ）";
+            this.umaFileSaveButton.UseVisualStyleBackColor = true;
+            this.umaFileSaveButton.Click += new System.EventHandler(this.umaFileSaveButton_Click);
             // 
             // raceFileSaveButton
             // 
@@ -170,7 +181,6 @@
             this.umaGroupBox.Controls.Add(this.umaPostButton);
             this.umaGroupBox.Controls.Add(this.fromLabel);
             this.umaGroupBox.Controls.Add(this.fromTextBox);
-            this.umaGroupBox.Controls.Add(this.isSetupCheckBox);
             this.umaGroupBox.Location = new System.Drawing.Point(12, 175);
             this.umaGroupBox.Name = "umaGroupBox";
             this.umaGroupBox.Size = new System.Drawing.Size(494, 88);
@@ -178,15 +188,15 @@
             this.umaGroupBox.TabStop = false;
             this.umaGroupBox.Text = "馬情報";
             // 
-            // umaFileSaveButton
+            // umaPostButton
             // 
-            this.umaFileSaveButton.Location = new System.Drawing.Point(246, 18);
-            this.umaFileSaveButton.Name = "umaFileSaveButton";
-            this.umaFileSaveButton.Size = new System.Drawing.Size(114, 48);
-            this.umaFileSaveButton.TabIndex = 3;
-            this.umaFileSaveButton.Text = "馬情報\r\n（ファイル保存のみ）";
-            this.umaFileSaveButton.UseVisualStyleBackColor = true;
-            this.umaFileSaveButton.Click += new System.EventHandler(this.umaFileSaveButton_Click);
+            this.umaPostButton.Location = new System.Drawing.Point(6, 34);
+            this.umaPostButton.Name = "umaPostButton";
+            this.umaPostButton.Size = new System.Drawing.Size(114, 48);
+            this.umaPostButton.TabIndex = 2;
+            this.umaPostButton.Text = "馬情報\r\n（WEB登録）";
+            this.umaPostButton.UseVisualStyleBackColor = true;
+            this.umaPostButton.Click += new System.EventHandler(this.umaPostButton_Click);
             // 
             // fromLabel
             // 
@@ -208,11 +218,11 @@
             // isSetupCheckBox
             // 
             this.isSetupCheckBox.AutoSize = true;
-            this.isSetupCheckBox.Location = new System.Drawing.Point(132, 49);
+            this.isSetupCheckBox.Location = new System.Drawing.Point(256, 72);
             this.isSetupCheckBox.Name = "isSetupCheckBox";
-            this.isSetupCheckBox.Size = new System.Drawing.Size(350, 16);
+            this.isSetupCheckBox.Size = new System.Drawing.Size(188, 28);
             this.isSetupCheckBox.TabIndex = 3;
-            this.isSetupCheckBox.Text = "初期化して一からデータを作成する（２時間近くかかる場合があります）";
+            this.isSetupCheckBox.Text = "初期化して一からデータを作成する\r\n（２時間近くかかる場合があります）";
             this.isSetupCheckBox.UseVisualStyleBackColor = true;
             // 
             // quickGroupBox
@@ -454,21 +464,11 @@
             this.mainBackgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.mainBackgroundWorker_ProgressChanged);
             this.mainBackgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.mainBackgroundWorker_RunWorkerCompleted);
             // 
-            // umaPostButton
-            // 
-            this.umaPostButton.Location = new System.Drawing.Point(6, 30);
-            this.umaPostButton.Name = "umaPostButton";
-            this.umaPostButton.Size = new System.Drawing.Size(114, 48);
-            this.umaPostButton.TabIndex = 2;
-            this.umaPostButton.Text = "馬情報\r\n（WEB登録）";
-            this.umaPostButton.UseVisualStyleBackColor = true;
-            this.umaPostButton.Click += new System.EventHandler(this.umaPostButton_Click);
-            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(524, 403);
+            this.ClientSize = new System.Drawing.Size(524, 451);
             this.Controls.Add(this.mainToolStripContainer);
             this.Name = "MainForm";
             this.Text = "JV-Relay";
@@ -482,6 +482,7 @@
             this.mainStatusStrip.ResumeLayout(false);
             this.mainStatusStrip.PerformLayout();
             this.debugGroupBox.ResumeLayout(false);
+            this.debugGroupBox.PerformLayout();
             this.umaGroupBox.ResumeLayout(false);
             this.umaGroupBox.PerformLayout();
             this.quickGroupBox.ResumeLayout(false);
