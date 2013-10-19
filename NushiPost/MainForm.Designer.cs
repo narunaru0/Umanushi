52671f2fafb2bcbd4a7c5f7922a9a94482c82d95
narunaru0 <h-hirahara@mba.nifty.ne.jp>
2013-10-19 22:22:32 +0900

    NushiPostβ版

---
+++ NushiPost/MainForm.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace NushiPost
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.bodyTextBox = new System.Windows.Forms.TextBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.postButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.subjectTextBox = new System.Windows.Forms.TextBox();
+            this.subjectLabel = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.toTextBox = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.toLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.panel2);
+            this.splitContainer1.Panel1.Controls.Add(this.panel4);
+            this.splitContainer1.Panel1.Controls.Add(this.panel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.toTextBox);
+            this.splitContainer1.Panel2.Controls.Add(this.panel3);
+            this.splitContainer1.Size = new System.Drawing.Size(630, 411);
+            this.splitContainer1.SplitterDistance = 453;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.bodyTextBox);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 52);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(453, 316);
+            this.panel2.TabIndex = 1;
+            // 
+            // bodyTextBox
+            // 
+            this.bodyTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bodyTextBox.Location = new System.Drawing.Point(0, 0);
+            this.bodyTextBox.Multiline = true;
+            this.bodyTextBox.Name = "bodyTextBox";
+            this.bodyTextBox.Size = new System.Drawing.Size(453, 316);
+            this.bodyTextBox.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.postButton);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(0, 368);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(453, 43);
+            this.panel4.TabIndex = 2;
+            // 
+            // postButton
+            // 
+            this.postButton.Location = new System.Drawing.Point(13, 7);
+            this.postButton.Name = "postButton";
+            this.postButton.Size = new System.Drawing.Size(75, 23);
+            this.postButton.TabIndex = 0;
+            this.postButton.Text = "送信";
+            this.postButton.UseVisualStyleBackColor = true;
+            this.postButton.Click += new System.EventHandler(this.postButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.subjectTextBox);
+            this.panel1.Controls.Add(this.subjectLabel);
+            this.panel1.Controls.Add(this.passwordTextBox);
+            this.panel1.Controls.Add(this.passwordLabel);
+            this.panel1.Controls.Add(this.nameTextBox);
+            this.panel1.Controls.Add(this.nameLabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(453, 52);
+            this.panel1.TabIndex = 0;
+            // 
+            // subjectTextBox
+            // 
+            this.subjectTextBox.Location = new System.Drawing.Point(69, 29);
+            this.subjectTextBox.Name = "subjectTextBox";
+            this.subjectTextBox.Size = new System.Drawing.Size(367, 19);
+            this.subjectTextBox.TabIndex = 5;
+            // 
+            // subjectLabel
+            // 
+            this.subjectLabel.AutoSize = true;
+            this.subjectLabel.Location = new System.Drawing.Point(5, 32);
+            this.subjectLabel.Name = "subjectLabel";
+            this.subjectLabel.Size = new System.Drawing.Size(35, 12);
+            this.subjectLabel.TabIndex = 4;
+            this.subjectLabel.Text = "題名：";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(252, 4);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(100, 19);
+            this.passwordTextBox.TabIndex = 3;
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(188, 7);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(58, 12);
+            this.passwordLabel.TabIndex = 2;
+            this.passwordLabel.Text = "パスワード：";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(69, 4);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 19);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(5, 7);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(58, 12);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "うまぬし名：";
+            // 
+            // toTextBox
+            // 
+            this.toTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toTextBox.Location = new System.Drawing.Point(0, 33);
+            this.toTextBox.Multiline = true;
+            this.toTextBox.Name = "toTextBox";
+            this.toTextBox.Size = new System.Drawing.Size(173, 378);
+            this.toTextBox.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.toLabel);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(173, 33);
+            this.panel3.TabIndex = 1;
+            // 
+            // toLabel
+            // 
+            this.toLabel.AutoSize = true;
+            this.toLabel.Location = new System.Drawing.Point(56, 7);
+            this.toLabel.Name = "toLabel";
+            this.toLabel.Size = new System.Drawing.Size(53, 12);
+            this.toLabel.TabIndex = 3;
+            this.toLabel.Text = "宛先リスト";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 411);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "MainForm";
+            this.Text = "NushiPost";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox bodyTextBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox subjectTextBox;
+        private System.Windows.Forms.Label subjectLabel;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label toLabel;
+        private System.Windows.Forms.TextBox toTextBox;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button postButton;
+    }
+}
+
---
+++ NushiPost/MainForm.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NushiPost
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void postButton_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            string url;
+            string errorMessage = string.Empty;
+
+            NameValueCollection querystring = new NameValueCollection();
+            CookieContainer cookies = new CookieContainer();
+
+            // 認証
+            url = "http://www.umanushi.com/old2/mail_form/";
+            querystring["usr"] = nameTextBox.Text;
+            querystring["pwd"] = passwordTextBox.Text;
+            result = HttpPost(url, querystring, cookies);
+            if (cookies.Count == 0)
+            {
+                // 認証に失敗
+                errorMessage = string.Format("ログインに失敗しました。うまぬし名=[{1}]、パスワード=[{2}]",
+                    querystring["usr"],
+                    querystring["pwd"]);
+                return;
+            }
+            querystring.Clear();
+
+            // 送信
+            foreach (var to in toTextBox.Text.Replace("\r\n", "\n").Split('\n'))
+            {
+                url = "http://www.umanushi.com/old2/mail_form/";
+                querystring["mode"] = "mail_go";
+                querystring["speed"] = "mid";
+                querystring["usr"] = nameTextBox.Text;
+                querystring["pwd"] = passwordTextBox.Text;
+                querystring["subject"] = subjectTextBox.Text;
+                querystring["from"] = nameTextBox.Text;
+                querystring["to"] = to;
+                querystring["body"] = bodyTextBox.Text;
+                result = HttpPost(url, querystring, cookies);
+
+                System.Threading.Thread.Sleep(5000);
+            }
+        }
+
+        /// <summary>
+        /// HTTPでのPOST処理
+        /// </summary>
+        /// <param name="url">対象URL</param>
+        /// <param name="querystring">POSTパラメータ</param>
+        /// <param name="cookies">POST時に使用するCookie</param>
+        /// <returns>HTTPのPOST結果</returns>
+        public string HttpPost(string url, NameValueCollection querystring, CookieContainer cookies)
+        {
+            string param = "";
+            if (querystring != null)
+            {
+                foreach (string key in querystring.Keys)
+                {
+                    param += String.Format("{0}={1}&", key, querystring.Get(key));
+                }
+            }
+            byte[] data = Encoding.GetEncoding("Shift-JIS").GetBytes(param);
+
+            // リクエストの作成
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            req.Method = "POST";
+            req.ContentType = "application/x-www-form-urlencoded";
+            req.ContentLength = data.Length;
+            req.CookieContainer = cookies;
+
+            // ポスト・データの書き込み
+            Stream reqStream = req.GetRequestStream();
+            reqStream.Write(data, 0, data.Length);
+            reqStream.Close();
+
+            WebResponse res = req.GetResponse();
+
+            // レスポンスの読み取り
+            Stream resStream = res.GetResponseStream();
+            StreamReader sr = new StreamReader(resStream, Encoding.GetEncoding("Shift-JIS"));
+            string result = sr.ReadToEnd();
+            sr.Close();
+            resStream.Close();
+
+            return result;
+        }    
+    }
+}
